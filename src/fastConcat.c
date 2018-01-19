38123e9f1a060829e9bb779e9e88d975c7bab65c
Matt Summersgill <matt.summersgill@hpidc.com>
2018-01-19 10:02:08 -0600

    Trying to simplify call by allocating internally where possible

--- src/fastConcat.c
+++ src/fastConcat.c
@@ -36,44 +36,45 @@ void writeInt32(int *col, size_t row, char **pch)
 
 //end of data.table code. 
 
-SEXP CfastConcat ( SEXP x, SEXP preallocated_target, SEXP columns, SEXP start_row, SEXP end_row, SEXP sep ) {
+SEXP CfastConcat ( SEXP l, SEXP columns, SEXP sep ) {
+  
   const char * _sep = CHAR(asChar(sep));
   int _has_sep = strcmp(_sep,"");
   
-  const size_t _start_row = INTEGER(start_row)[0] - 1;
-  const size_t _end_row = INTEGER(end_row)[0];
+  R_len_t nrow = length(VECTOR_ELT(l,0)), ncol = length(l);
+  SEXP _result = PROTECT(allocVector(STRSXP, nrow));
+  
+
   
   const int max_out_len = 256 * 256; //max length of the final string
   char buffer[max_out_len];
-  const size_t num_elements = _end_row - _start_row;
-  const size_t num_columns = LENGTH(columns);
   const int * _columns = INTEGER(columns);
   
   
-  if (_has_sep == 0) { // skip writing delimiter
-    for(size_t i = _start_row; i < _end_row; ++i) {
+  //if (_has_sep == 0) { // skip writing delimiter
+  //  for(int i = 1; i < nrow; ++i) {
+  //    char *buf_pos = buffer;
+  //    for(size_t c = 0; c < num_columns; ++c) {
+  //      writeInt32(INTEGER(VECTOR_ELT(x, _columns[c] - 1)), i, &buf_pos);
+  //    }
+  //    SET_STRING_ELT(_result,i, mkCharLen(buffer, buf_pos - buffer));
+  //  }
+  //  
+  //} else { // write passed delimiter
+    for(int i = 1; i < nrow; ++i) {
       char *buf_pos = buffer;
-      for(size_t c = 0; c < num_columns; ++c) {
-        writeInt32(INTEGER(VECTOR_ELT(x, _columns[c] - 1)), i, &buf_pos);
-      }
-      SET_STRING_ELT(preallocated_target,i, mkCharLen(buffer, buf_pos - buffer));
-    }
-    
-  } else { // write passed delimiter
-    for(size_t i = _start_row; i < _end_row; ++i) {
-      char *buf_pos = buffer;
-      for(size_t c = 0; c < num_columns; ++c) {
+      for(int c = 0; c < ncol; ++c) {
         if(c > 0) {
           buf_pos[0] = *_sep;
           ++buf_pos;
         }
-        writeInt32(INTEGER(VECTOR_ELT(x, _columns[c] - 1)), i, &buf_pos);
+        writeInt32(INTEGER(VECTOR_ELT(l, _columns[c] - 1)), i, &buf_pos);
       }
-      SET_STRING_ELT(preallocated_target,i, mkCharLen(buffer, buf_pos - buffer));
+      SET_STRING_ELT(_result,i, mkCharLen(buffer, buf_pos - buffer));
     }
-  }
+  //}
   
-  return preallocated_target;
+  return _result;
   
   warning("your C program does not return anything!");
   return R_NilValue;
