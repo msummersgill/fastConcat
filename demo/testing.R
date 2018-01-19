b5a32731f1a5a8f577a1c5648142657e961f3182
Matt Summersgill <matt.summersgill@hpidc.com>
2018-01-19 06:45:45 -0600

    Clear out unnecessary Rcpp Imports now that only C is used

--- demo/testing.R
+++ demo/testing.R
@@ -1,7 +1,7 @@
 library(fastConcat)
 library(data.table)
 
-RowCount <- 5
+RowCount <- RowCount <- 5
 DT <- data.table(x = "foo",
                  y = "bar",
                  a = sample.int(9, RowCount, TRUE),
@@ -16,12 +16,37 @@ ConcatCols <- list("a","b","c","d","e","f")
 ## Do it 3x as many times
 ConcatCols <- c(ConcatCols,ConcatCols,ConcatCols)
 
+## using fastConcat::concat
+
 preallocated_target <- character(RowCount)
 column_indices <- sapply(ConcatCols, FUN = function(x) { which(colnames(DT) == x )})
-
 fastConcat::concat(DT, preallocated_target, column_indices, as.integer(1), as.integer(RowCount))
 
 
 
+# Full 5 million rows -----------------------------------------------------
+
+library(fastConcat)
+library(data.table)
+
+RowCount <- RowCount <- 5e6
+DT <- data.table(x = "foo",
+                 y = "bar",
+                 a = sample.int(9, RowCount, TRUE),
+                 b = sample.int(9, RowCount, TRUE),
+                 c = sample.int(9, RowCount, TRUE),
+                 d = sample.int(9, RowCount, TRUE),
+                 e = sample.int(9, RowCount, TRUE),
+                 f = sample.int(9, RowCount, TRUE))
+
+## Generate an expression to paste an arbitrary list of columns together
+ConcatCols <- list("a","b","c","d","e","f")
+## Do it 3x as many times
+ConcatCols <- c(ConcatCols,ConcatCols,ConcatCols)
+
+## using fastConcat::concat
+
+preallocated_target <- character(RowCount)
+column_indices <- sapply(ConcatCols, FUN = function(x) { which(colnames(DT) == x )})
+
 DT[, State := fastConcat::concat(DT, preallocated_target, column_indices, as.integer(1), as.integer(RowCount))]
-DT
